c19ac582e5989d704d2435ec48d98f3353560ea6
Krishna Karnamadakala <krishna@hindulit.com>
2022-01-22 08:36:27 -0700

    Tried to make changes to match ZTM course

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,7 @@
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 const socket = io('http://localhost:3000');
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
@@ -89,6 +90,12 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score,
+  });
 }
 
 // Adjust Ball Movement
@@ -99,6 +106,11 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score,
+  });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -154,19 +166,28 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
+}
+
+function loadGame() {
+  createCanvas();
+  renderIntro();
+  socket.emit('ready');
 }
 
 // Start Game, Reset Everything
 function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
+  window.requestAnimationFrame(animate);
   createCanvas();
   renderIntro();
+  socket.emit('ready');
   
-  paddleIndex = 0;
-  window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
     playerMoved = true;
     paddleX[paddleIndex] = e.offsetX;
@@ -176,11 +197,35 @@ function startGame() {
     if (paddleX[paddleIndex] > (width - paddleWidth)) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+
+    socket.emit('paddleMove', {
+      xPosition: paddleX[paddleIndex],
+    });
     // Hide Cursor
     canvas.style.cursor = 'none';
   });
 }
 
 // On Load
-startGame();
+loadGame();
 
+socket.on('connect', () => {
+  console.log('Connected as...', socket.id);
+});
+
+socket.on('startGame', (refereeId) => {
+  console.log('Referee is', refereeId);
+
+  isReferee = socket.id === refereeId;
+  startGame();
+});
+
+socket.on('paddleMove', (paddleData) => {
+  // Toggle 1 into 0, and 0 into 1
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
+});
+
+socket.on('ballMove', (ballData) => {
+  ({ ballX, ballY, score } = ballData);
+});
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const io = require('socket.io')(server, {
 const PORT = 3000;
 
 server.listen(PORT);
-console.log(`Listening on ${PORT}...`);
+console.log(`Listening on port ${PORT}...`);
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  console.log('a user connected');
 });
